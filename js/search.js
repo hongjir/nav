3061ac44c64b3e145f9cd7036b5b63a39dc0c5b9
hongjir <hongjir>
2022-08-10 16:09:32 +0800

    version 1.1

---
+++ js/search.js
@@ -0,0 +1,22 @@
+var google = "https://www.google.com/search?q="
+var bing = "https://cn.bing.com/search?q="
+var weibo = "https://weibo.com/search?q="
+var zhihu = "https://www.zhihu.com/search?type=content&q="
+
+var engine = bing
+
+function re_search(){
+    var event = window.event || arguments.callee.caller.arguments[0];  
+    if (event.keyCode == 13)
+    {
+    search();
+    }
+}
+
+function search(){
+    var key = document.getElementById("key").value
+    if (key){
+        var url = engine + key
+        window.open(url, '_blank')
+    }
+}
